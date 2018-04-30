73e14aa1eedd17dffb9f725f7467e859945cc01b
Okulich <ivanokulichev@gmail.com>
2018-04-30 18:25:10 +0200

    comments

--- mysql/packet.go
+++ mysql/packet.go
@@ -2,14 +2,18 @@ package mysql
 
 import (
 	"errors"
+	"fmt"
 	"io"
 	"net"
 )
 
 const (
+	COM_QUIT         = 1
 	COM_QUERY        = 3
 	COM_STMT_PREPARE = 22
 )
+
+var counter = 1
 
 // Determiner les erreurs possibles
 var ErrWritePacket = errors.New("error while writing packet payload")
@@ -20,6 +24,28 @@ func ProxyPacket(src, dst net.Conn) ([]byte, error) {
 	pkt, err := ReadPacket(src)
 	if err != nil {
 		return nil, err
+	}
+
+	fmt.Println(counter, " : %x", pkt)
+	counter++
+
+	// check if packet is querry
+	if query, err := GetQueryString(pkt); err == nil {
+		// lauch scan
+		// if normal --> manage
+		// *	*	*	*	get information if select/insert/update/delete
+		// *	*	*	*	if select do nothing
+		// *	*	*	*	if insert/update/delete --> see crem
+		// if custom --> manage
+		// *	*	*	*	only select here !!!
+		// *	*	*	*	get info after ***
+		// *	*	*	*	create second part of querry
+		// *	*	*	*	create first part of querry, and append()
+		// *	*	*	*	close connection to mysql
+		// *	*	*	*	open connection to columnStore
+		// *	*	*	*	get data & transmit them
+		// *	*	*	*	return !
+		fmt.Println("Query --> ", query)
 	}
 
 	_, err = WritePacket(pkt, dst)
@@ -40,7 +66,13 @@ func ReadPacket(conn net.Conn) ([]byte, error) {
 		return nil, err
 	}
 
+	fmt.Println(header[0], header[1], header[2], header[3])
+
 	bodyLength := int(uint32(header[0]) | uint32(header[1])<<8 | uint32(header[2])<<16)
+	// 00000000 00000000 00000000 00101011 --> h[0]
+	// 00000000 00000000 01101000 00000000 --> h[1]<<8
+	// 00000000 00101111 00000000 00000000 --> h[2]<<16
+	// 00000000 00101111 01101000 00101011 --> bodyLength
 
 	body := make([]byte, bodyLength)
 
@@ -76,4 +108,8 @@ func GetQueryString(pkt []byte) (string, error) {
 	}
 
 	return "", ErrNoQueryPacket
+}
+
+func Clean() {
+	counter = 1
 }
