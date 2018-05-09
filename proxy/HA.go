ade0fd3cef406ff46290199054e28db8d70f42c6
Okulich <ivanokulichev@gmail.com>
2018-05-10 01:36:34 +0200

    catch UPDATE query and its parameters

--- proxy/HA.go
+++ proxy/HA.go
@@ -10,9 +10,10 @@ import (
 
 func PerformInsertQuery(query string) {
 
+	// sql = "INSERT INTO persons (name, age) VALUES('type1', 15)"
+
 	// get query slice
 	okQuery := strings.TrimSpace(query)
-	fmt.Println("\nEnter fct, query:", okQuery)
 
 	// get indexes
 	initIndex := len("INSERT INTO")
@@ -59,33 +60,83 @@ func PerformInsertQuery(query string) {
 	db.Close()
 	fmt.Println(id)
 
-	// final query
-	//qr := "INSERT INTO " + tableName + " ("
-
-	// connect HA
 	/*
-		db, err := sql.Open("mysql", "okulich:22048o@tcp(192.168.1.115)/okidb")
-		if err != nil {
-			panic(err.Error())
-		}
-		defer db.Close()
-	*/
+		// final query
+		//qr := "INSERT INTO " + tableName + " ("
 
-	/*
-		// executer la requette
-		_, errex := db.Exec(query)
-		if errex != nil {
-			panic(errex.Error())
-		}
+		// connect HA
+
+			db, err := sql.Open("mysql", "okulich:22048o@tcp(192.168.1.115)/okidb")
+			if err != nil {
+				panic(err.Error())
+			}
+			defer db.Close()
+
+			// executer la requette
+			_, errex := db.Exec(query)
+			if errex != nil {
+				panic(errex.Error())
+			}
 	*/
 }
 
 func PerformUpdateQuery(query string) {
 
-	// start := time.Now()
-}
-func PerformDeleteQuery(query string) {
+	// sql = "UPDATE MyGuests SET lastname='Doe' WHERE id=2"
+
+	// get query slice
+	okQuery := strings.TrimSpace(query)
+	fmt.Println("\nEnter fct, query:", okQuery)
+
+	// get indexes
+	initIndex := len("UPDATE")
+	setIndex := strings.Index(okQuery, "SET")
+	whereIndex := strings.Index(okQuery, "WHERE")
+
+	// get table name
+	tableName := strings.TrimSpace(okQuery[initIndex:setIndex])
+	fmt.Println("tablename:", tableName)
+
+	// get columns
+	valueParams := strings.Split(strings.TrimSpace(okQuery[setIndex+3:whereIndex]), "=")
+	for i := range valueParams {
+		valueParams[i] = strings.Trim(valueParams[i], " ")
+		fmt.Println("valueParam:", valueParams[i])
+	}
+
+	// get id
+	idParams := strings.Split(strings.TrimSpace(okQuery[whereIndex+5:len(okQuery)]), "=")
+	for i := range idParams {
+		idParams[i] = strings.Trim(idParams[i], " ")
+		fmt.Println("idParam:", idParams[i])
+	}
+	//id := idParams[1]
+
+	/*
+		// final query
+		//qr := "INSERT INTO " + tableName + " ("
+
+		// connect HA
+
+			db, err := sql.Open("mysql", "okulich:22048o@tcp(192.168.1.115)/okidb")
+			if err != nil {
+				panic(err.Error())
+			}
+			defer db.Close()
+
+			// executer la requette
+			_, errex := db.Exec(query)
+			if errex != nil {
+				panic(errex.Error())
+			}
+	*/
 
 }
 func PerformSelectQuery(query string) {
+
+	// sql = "SELECT id, firstname, lastname, (*) FROM MyGuests WHERE id=45 HISTORY BETWEEN t1, t2"
+}
+
+func PerformDeleteQuery(query string) {
+
 }
