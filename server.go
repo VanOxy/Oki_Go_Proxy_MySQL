b95d693d9c9ffbd46e5e61301f5aad164a4a0d07
Okulich <ivanokulichev@gmail.com>
2018-05-28 18:24:39 +0200

    Initialisation done: Connect relative db, get tables. Connect columnStore, create tables if not exists.

--- server.go
+++ server.go
@@ -1,23 +1,29 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"net"
+	"os"
 
-	helper "./helper"
-	dbms "./mysql"
+	dbms "./proxy"
 )
+
 /*
 SHA1( password ) XOR SHA1( "20-bytes random data from server" <concat> SHA1( SHA1( password ) ) )
 */
 const (
-	MYSQL = "192.168.1.115:3306"	// MaxScale
+	MYSQL = "192.168.1.115:3306" // MaxScale
 	PROXY = "192.168.1.100:3306" // THIS SERVER
 )
 
 func main() {
+	Initialisation()
+	fmt.Println("Before exit")
+	os.Exit(1)
 
+	// listen to port
 	listener, err := net.Listen("tcp", PROXY)
 	if err != nil {
 		log.Fatalf("%s: %s", "ERROR", err.Error())
@@ -26,6 +32,7 @@ func main() {
 
 	fmt.Println("Proxy started at : ", PROXY)
 
+	// connection arrived
 	for {
 		conn, err := listener.Accept()
 		if err != nil {
@@ -34,6 +41,7 @@ func main() {
 
 		fmt.Println("connection accepted...")
 
+		// launch in other thread
 		go handleConnection(conn)
 	}
 }
@@ -72,7 +80,6 @@ func appToMysql(client net.Conn, mysql net.Conn) {
 	for {
 		err := dbms.ProxyPacket(client, mysql)
 		if err != nil {
-			dbms.Clean()
 			break
 		}
 	}
@@ -83,14 +90,99 @@ func MysqlToApp(mysql net.Conn, client net.Conn) {
 	for {
 		err := dbms.ProxyPacket(mysql, client)
 		if err != nil {
-			dbms.Clean()
 			break
 		}
 	}
 }
 
-func testFunc() {
-	query := "DROP DATABASE okidb"
-	packet := helper.StringToBytes(query)
-	fmt.Println(packet)
+// create new db if not exist as well, as tables in fucntion of RDBMS tables
+func Initialisation() {
+	// *** GET TABLES FROM RELATIONAL ***
+	// connect to maxScale
+	db_mxsc, err := sql.Open("mysql", "okulich:22048o@tcp(192.168.1.115)/okidb")
+	if err != nil {
+		panic(err.Error())
+	}
+	defer db_mxsc.Close()
+
+	// query
+	rows, err := db_mxsc.Query("SHOW TABLES FROM okidb")
+	if err != nil {
+		panic(err.Error())
+	}
+
+	// Get column names
+	columns, err := rows.Columns()
+	if err != nil {
+		panic(err.Error())
+	}
+
+	// Make a slice for the values
+	values := make([]sql.RawBytes, len(columns))
+
+	scanArgs := make([]interface{}, len(values))
+	for i := range values {
+		scanArgs[i] = &values[i]
+	}
+
+	// tables
+	var tables []string
+
+	// Fetch rows
+	for rows.Next() {
+		// get RawBytes from data
+		err = rows.Scan(scanArgs...)
+		if err != nil {
+			panic(err.Error())
+		}
+		// Now do something with the data.
+		// Here we just print each column as a string.
+		var value string
+		for i := range values {
+			// Here we can check if the value is nil (NULL value)
+			if values[i] == nil {
+				value = "NULL"
+			} else {
+				value = string(values[i])
+				tables = append(tables, value)
+			}
+		}
+	}
+	if err = rows.Err(); err != nil {
+		panic(err.Error()) // proper error handling instead of panic in your app
+	}
+
+	fmt.Println("Show tables:")
+	for i := range tables {
+		fmt.Println(tables[i])
+	}
+
+	// *** CREATE DB & TABLES INTO ColumnStore ***
+	db_mcs, err := sql.Open("mysql", "okulich:22048o@tcp(192.168.1.121)/")
+	if err != nil {
+		panic(err.Error())
+	}
+	defer db_mcs.Close()
+
+	_, err = db_mcs.Exec("CREATE DATABASE IF NOT EXISTS okidb")
+	if err != nil {
+		panic(err)
+	}
+
+	_, err = db_mcs.Exec("USE okidb")
+	if err != nil {
+		panic(err)
+	}
+
+	// create tables
+	for i := range tables {
+		query := "CREATE TABLE IF NOT EXISTS " + tables[i] + " (id INT NOT NULL COMMENT 'autoincrement=1', column_name VARCHAR(30) NOT NULL, value VARCHAR(50) NOT NULL, reg_date DATETIME NOT NULL) engine=columnstore"
+
+		_, err = db_mcs.Exec(query)
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	tables = nil
 }
